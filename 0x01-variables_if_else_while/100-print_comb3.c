57bb2f185b03fe0de09b40c91f12c6d866ca91ae
obesampson80 <obesampson80@gmail.com>
2023-06-16 18:19:02 -0700

    print com3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all possible different
+ * combinations of two digits
+ * Return: 0 program success
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = 1; b < 10; b++)
+		{
+			if ((a + b) != 17)
+			{
+				if (a != b)
+				{
+					if ((a + b) != (b + a))
+					{
+						putchar('0' + a);
+						putchar('0' + b);
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
