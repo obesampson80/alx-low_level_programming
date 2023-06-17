e4ddc49f6371f5a0ff918ec3367262d6e73fecb5
obesampson80 <obesampson80@gmail.com>
2023-06-16 17:51:35 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print 0 - 9 combination
+ * Return: 0 program execute successfully
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
