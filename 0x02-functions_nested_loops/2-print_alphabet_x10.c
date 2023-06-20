755fac0c4185675e445b4554beda12472e694bf3
obesampson80 <obesampson80@gmail.com>
2023-06-20 13:41:03 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,10 +13,10 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for ( az = 'a'; az <= 'z'; az++)
+		for (az = 'a'; az <= 'z'; az++)
 		{
 			_putchar(az);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
