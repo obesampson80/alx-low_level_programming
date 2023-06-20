4a9b5020b75d6f7b2c96de78358dad6f61e3823e
obesampson80 <obesampson80@gmail.com>
2023-06-20 16:44:08 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * -abs - Function that computes the absolute value
+ * _abs - Function that computes the absolute value
  *  of an integer
  *
  *  Return: int value without sign
