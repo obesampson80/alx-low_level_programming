a4cba6569911a496ca5a009acc991d3bc028402b
obesampson80 <obesampson80@gmail.com>
2023-06-20 16:41:56 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * -abs - Function that computes the absolute value
+ *  of an integer
+ *
+ *  Return: int value without sign
+ */
+int _abs i(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}
