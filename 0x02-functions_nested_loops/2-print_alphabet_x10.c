7d8d6d756086240084fa1cd5b4340f6ae5423d72
obesampson80 <obesampson80@gmail.com>
2023-06-20 13:37:55 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Function that prints a-z 10 times
+ * followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char az;
+
+	for (i = 0; i < 10; i++)
+	{
+		for ( az = 'a'; az <= 'z'; az++)
+		{
+			_putchar(az);
+		}
+	}
+	_putchar('\n');
+}
