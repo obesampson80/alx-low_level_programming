9b7dc8edbfbdd67df83b784b6d54ccc7c7b2859d
obesampson80 <obesampson80@gmail.com>
2023-06-20 16:02:36 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		ret = 1;
+		ret = +1;
 	}
 	else if (n == 0)
 	{
@@ -22,5 +22,5 @@ int print_sign(int n)
 	{
 		ret = -1;
 	}
-	return ret;
+	return (ret);
 }
