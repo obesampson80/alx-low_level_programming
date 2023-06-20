c4ea0d1b53babd247a0b0ff0068fbbe20d2887b3
obesampson80 <obesampson80@gmail.com>
2023-06-20 03:52:42 -0700

    Output _putchar using unistd.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include "_putchar.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+/**
+ * output - takes a string and prints it using _putchar function
+ * followed by a newline
+ * @str: The string reference
+ *
+ * Return: void
+*/
+
+void output(char *str)
+{
+        int i = 0;
+
+        while (str[i] != '\0')
+        {
+                _putchar(str[i]);
+                i++;
+        }
+}
+
+/**
+ * main - call the output function and output _output
+ * followed by a new line
+ *
+ * Return: On success 0
+ */
+
+int main(void)
+{
+        char *str = "_putchar\n";
+
+        output(str);
+
+        return (0);
+}
