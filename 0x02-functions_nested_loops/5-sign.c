be9974b569c0ca0cddcba9342a60b9273821c0f9
obesampson80 <obesampson80@gmail.com>
2023-06-20 15:59:10 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Function that prints the sign of a number
+ * @n: int value to check sign
+ *
+ * Return: 1 for +, 0 for 0 and -1 for -
+ */
+int print_sign(int n)
+{
+	int ret;
+
+	if (n > 0)
+	{
+		ret = 1;
+	}
+	else if (n == 0)
+	{
+		ret = 0;
+	}
+	else
+	{
+		ret = -1;
+	}
+	return ret;
+}
