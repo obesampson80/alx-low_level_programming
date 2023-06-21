9dedc198300546193c082f0b64d09d7ef42a0692
obesampson80 <obesampson80@gmail.com>
2023-06-20 18:44:39 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,12 +10,23 @@ void times_table(void)
 {
 	int a;
 	int b;
+	int tmp;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			_putchar((a * b) + '0');
+			tmp = a * b;
+
+			if ((tmp % 10) != 0)
+			{
+				_putchar((tmp / 10) + '0');
+				_putchar((tmp % 10) + '0');
+			}
+			else
+			{
+				_putchar(tmp + '0');
+			}
 			if (b != 9)
 			{
 				_putchar(',');
