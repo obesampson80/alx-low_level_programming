66ab91db7674dd408ecc610835d2407392bb6dc5
obesampson80 <obesampson80@gmail.com>
2023-06-22 15:29:36 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,24 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (n % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else if (n % 3 == 0 && n % 5 == 0)
+		{
 			printf("FizzBuzz");
+		}
 		else
+		{
 			printf(n);
+		}
 		printf(" ");
 	}
+	printf("\n");
 
 	return (0);
 }
