2fe4556c59b106bbed1c05edb78f7f92003db29a
obesampson80 <obesampson80@gmail.com>
2023-06-22 14:38:16 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,7 @@
 void print_line(int n)
 {
 	int i;
+	int j;
 
 	if (n <= 0)
 	{
@@ -18,8 +19,12 @@ void print_line(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
 			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
