bbd25689d0d417db913bdf88be2259717afa546a
obesampson80 <obesampson80@gmail.com>
2023-06-22 16:47:10 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,16 +11,23 @@ void print_triangle(int size)
 	int j;
 	int k;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (j = i +  1; j < size; j++)
-		{
-			_putchar(' ');
-		}
-		for (k = i + 1; k <= i; k++)
-		{
-			_putchar(35);
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 	}
 }
