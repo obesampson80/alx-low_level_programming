404179e64f25257a66664de0b1227773a87db5c5
obesampson80 <obesampson80@gmail.com>
2023-06-22 15:26:00 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints Fizz-Buzz given an int range n
+ * Fizz if n%3==0, Buzz if n%5==0, fizzBuzz if both
+ * numbers otherwise - followed by a space
+ * Return: 0 always success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf(n);
+		printf(" ");
+	}
+
+	return (0);
+}
