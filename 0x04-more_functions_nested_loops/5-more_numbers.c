8ff2ffbff3531e82760889d186b2a454f1d19e4f
obesampson80 <obesampson80@gmail.com>
2023-06-22 12:58:06 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,23 +9,18 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	int n;
 
 	for (i = 0; i <= 10; i++)
 	{
-		for (j = 0; j <= 14; j++){
-			n = j;
+		for (j = 0; j <= 14; j++)
+		{
 
 			if (j > 9)
 			{
-				_putchar('1');
-				n = j % 10;
+				_putchar(j / 10 + '0');
 			}
-			else
-			{
-				_putchar((n + '0');
-			}
+			_putchar((j % 10 + '0');
 		}
-		_putchar(n + '0');
+		_putchar('\n');
 	}
 }
