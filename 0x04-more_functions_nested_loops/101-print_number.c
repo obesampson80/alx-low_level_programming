4d06608f47f348c9b1347ea1b8826500aeeb8467
obesampson80 <obesampson80@gmail.com>
2023-06-22 18:28:32 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints and integer
+ * using _putchar only
+ * @n: integer to print
+ * Return: void
+ */
+void print_number(int n)
+{
+	while (n / 10 != 0)
+	{
+		_putchar(n % 10 + '0');
+
+		n = n / 10;
+	}
+	_putchar('\n');
+}
