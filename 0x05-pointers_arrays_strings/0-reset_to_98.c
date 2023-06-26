5dbe5832c095b8914ca33b8c916d7aa39530ca2f
obesampson80 <obesampson80@gmail.com>
2023-06-26 13:10:35 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+ * reset_to_98 - set the integer to 98
+ * @n: integer we want to reset
+ * Return: nothing
+ */
+void reset_to_98(int n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#0x05. C - Pointers, arrays and strings
+*This directory contains solutions to projects on pointers, arrays and strings*
