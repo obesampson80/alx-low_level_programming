b364bd2194e8f02bdff1c454790e2f0931c182f0
obesampson80 <obesampson80@gmail.com>
2023-06-26 15:43:11 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "man.h"
+
+/**
+ * puts2 - function that prints every other character of a string,
+ * starting with the first character,
+ * followed by a new line
+ * @str: the string to be printed
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int x;
+	int y;
+
+	for (x = 0; str[x] != '\0'; x++)
+		continue;
+	for (y = 0; y < x; y += 2)
+		_putchar(str[y]);
+	_putchar('\n');
+}
