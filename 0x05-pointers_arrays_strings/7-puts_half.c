88f3f8071b3dde49659772f965ad16b756c56f64
obesampson80 <obesampson80@gmail.com>
2023-06-26 16:04:00 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 		for (z = x / 2; str[x] != '\0'; z++)
 			_putchar(str[z]);
 	}
-	else if
+	else if (x % 2)
 	{
 		for (y = (x - 1) / 2; y < x - 1; y++)
 			_putchar(str[y + 1]);
