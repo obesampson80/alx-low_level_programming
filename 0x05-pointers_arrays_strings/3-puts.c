0a8d754909b82b3e8082a209b202e2effad32ffc
obesampson80 <obesampson80@gmail.com>
2023-06-26 13:54:45 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a tring, followed by a newline to stdout
+ * @str: string to print
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(i);
+		i++;
+	}
+}
