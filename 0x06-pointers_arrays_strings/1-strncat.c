158aa2bbeb4b46f334b72223d78b475986024f27
obesampson80 <obesampson80@gmail.com>
2023-06-29 12:36:08 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* *_strcat - function that concatenates two strings
+* @dest: Destination string
+* @src: source string
+* @n: length of source string
+* Return: Destination string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, len_dest = 0;
+
+	while (dest[i++])
+		len_dest++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[len_dest++] = src[i];
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,3 @@
 `0-strcat.c` is a function that concatenates two strings
 
-
+`1-strncat.c` is a function that concatenates two strings using at most n bytes of src
