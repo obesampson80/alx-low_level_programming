4aa65135aff9a75c36f89be45ca8451a37eeb6d6
obesampson80 <obesampson80@gmail.com>
2023-06-29 13:44:31 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,7 +7,7 @@
 * @n: number of bytes to copy
 * Return: pointer to resulting string dest
 */
-char *_strncpy(char *dest, const char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
