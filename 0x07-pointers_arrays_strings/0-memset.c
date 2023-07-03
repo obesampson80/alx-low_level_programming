e32308d9508e2480eaa207fcde0cf266d4745eee
obesampson80 <obesampson80@gmail.com>
2023-07-03 13:35:35 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * _memset - function that fills memory with a constaant byte
- * @s: memory area to be filled
- * @b: constant byte
- * @n: number of bytes
- * Return: the pointer to the memory area s
- */
+* _memset - fills memory with a constant byte
+* @s: pointer to the memory area
+* @b: constant byte
+* @n: number of bytes to fill
+*
+* Return: pointer to the memory area
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
+	while (n--)
+		*(s + n) = b;
 	return (s);
 }
