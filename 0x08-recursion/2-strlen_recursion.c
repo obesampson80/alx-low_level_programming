1fe4642bf08fff35d0ada35644d603d905a30fae
obesampson80 <obesampson80@gmail.com>
2023-07-05 14:44:19 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
 * _strlen_recursion - returns the length of a string
-* @s: string whose length is to be found 
+* @s: string whose length is to be found
 * Return: length of the string
 */
 int _strlen_recursion(char *s)
