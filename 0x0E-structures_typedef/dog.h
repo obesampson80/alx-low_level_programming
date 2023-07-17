886e695439e0e3d6a41e9e297956bdcf541046ac
obesampson80 <obesampson80@gmail.com>
2023-07-17 14:21:59 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a new type describing a dog
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+*
+* Description: file that defines a new type struct dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
