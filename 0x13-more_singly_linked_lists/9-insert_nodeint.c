653b77c58cee589d25faba5fe51e749e6b25b2d6
obesampson80 <obesampson80@gmail.com>
2023-08-01 16:51:15 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (x = 0; temp && x < idx; x++)
+	for (x = 0; tmp && x < idx; x++)
 	{
 		if (x == idx - 1)
 		{
