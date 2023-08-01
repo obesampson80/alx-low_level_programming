1632b85a3350979c6cfae8fa1eaeae1613e6f33f
obesampson80 <obesampson80@gmail.com>
2023-08-01 15:08:08 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a linked list.
+ * @head: Pointer to the head of the linked list
+ * Return: Number of nodes in the list
+ */
+size_t print_list(const list_t *head)
+{
+    size_t node_count = 0;
+
+    while (head != NULL)
+    {
+        printf("%s\n", head->str);
+        node_count++;
+        head = head->next;
+    }
+
+    return (node_count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+*size_t print_listint(const listint_t *h)- a function that prints all the elements of a listint_t list.*
+
+
